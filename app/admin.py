f3db248169fb56fd2039ebddf46dcefde0c9ab69
Shehbaz <msmkhan1122@gmail.com>
2026-06-12 18:48:57 +0500

    added an admin page

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Deployment
-# Register your models here.
-admin.site.register(Deployment)
+
+@admin.register(Deployment)
+class DeploymentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'repo_name',
+        'status',
+        'container_id',
+        'created_at'
+    )
+
+    search_fields = (
+        'repo_name',
+        'status'
+    )
+
+    list_filter = (
+        'status',
+    )
